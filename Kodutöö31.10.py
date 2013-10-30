a70ed34cc757bdee8d63443b816e4ad95a6df1bd
Margus <margus@margus-mac.(none)>
2013-10-30 22:42:45 +0200

    Kodutööd

--- Kodutöö31.10.py
+++ Kodutöö31.10.py
@@ -2,22 +2,14 @@ import os
 clear = os.system('clear');
 nimi = raw_input('Sisestage oma nimi: ')
 perenimi = raw_input('Sisestage oma perenimi: ')
-kumb = raw_input('Kas nimi v6i perenimi? ')
 funktsioon = raw_input('Valige sobiv funktsioon: capitalize, swapcase, upper, lower, lstrip ')
-if funktsioon == 'lstrip':
-	lstrip_tahed = raw_input('Vali tahed: ')
-if kumb == 'nimi':
-	capitalize = nimi.capitalize()
-	swapcase = nimi.swapcase()
-	upper = nimi.upper()
-	lower = nimi.lower()
-	lstrip = nimi.lstrip(lstrip_tahed)
-if kumb == 'perenimi':
-	capitalize = perenimi.capitalize()
-	swapcase = perenimi.swapcase()
-	upper = perenimi.upper()
-	lower = perenimi.lower()
-	lstrip = perenimi.lstrip(lstrip_tahed)
+#if funktsioon == 'lstrip':
+#	lstrip_tahed = raw_input('Vali tahed: ')
+	
+capitalize = nimi.capitalize()
+swapcase = nimi.swapcase()
+upper = nimi.upper()
+lower = nimi.lower()
 
 if funktsioon == 'capitalize':
 	print capitalize
@@ -28,4 +20,6 @@ if funktsioon == 'upper':
 if funktsioon == 'lower':
 	print lower
 if funktsioon == 'lstrip':
+	lstrip_tahed = raw_input('Vali tahed: ')
+	lstrip = nimi.lstrip(lstrip_tahed)
 	print lstrip
