208641c9aed144bab3061a5774b9280ff56a57b4
Margus <margus@margus-mac.(none)>
2013-10-30 22:47:18 +0200

    Kodutööd

--- Kodutöö31.10.py
+++ Kodutöö31.10.py
@@ -1,11 +1,7 @@
 import os
 clear = os.system('clear');
 nimi = raw_input('Sisestage oma nimi: ')
-perenimi = raw_input('Sisestage oma perenimi: ')
 funktsioon = raw_input('Valige sobiv funktsioon: capitalize, swapcase, upper, lower, lstrip ')
-#if funktsioon == 'lstrip':
-#	lstrip_tahed = raw_input('Vali tahed: ')
-	
 capitalize = nimi.capitalize()
 swapcase = nimi.swapcase()
 upper = nimi.upper()
